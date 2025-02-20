6b69d667d36c1b283b35282140d05a8379920723
Vitor Menoli <vitoragario@gmail.com>
2025-02-19 23:30:19 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,47 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import RestaurantCategories from "./components/categories";
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: {
+      menuCategories: {
+        include: { products: true },
+      },
+    },
+  });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
+
+// http://localhost:3000/vm-donalds/menu?consumptionMethod=dine_in
