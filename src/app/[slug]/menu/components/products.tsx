dcc8a63c3ceb79c3c8f7ebae35e5840bfd33b106
Vitor Menoli <vitoragario@gmail.com>
2025-02-20 23:27:31 -0300

    feat: add product header component

---
+++ src/app/[slug]/menu/components/products.tsx
@@ -0,0 +1,48 @@
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+import { formatCurrency } from "@/helpers/format-currency";
+
+interface ProductsProps {
+  products: Product[];
+}
+
+const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  return (
+    <div className="space-y-3 px-5">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          href={`/${slug}/menu/${product.id}`}
+          className="flex items-center justify-between gap-10 border-b py-3"
+        >
+          {/* ESQUERDA */}
+          <div>
+            <h3 className="text-sm font-medium">{product.name}</h3>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}
+            </p>
+          </div>
+
+          {/* DIREITA */}
+          <div className="relative min-h-[82px] min-w-[120px]">
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-contain"
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
