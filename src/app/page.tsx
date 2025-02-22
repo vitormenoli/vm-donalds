4cbf20e5abccbf96fa33c883c54b0a2b36eb8656
Vitor Menoli <vitoragario@gmail.com>
2025-02-22 17:52:51 -0300

    chore: adjust home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,24 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const HomePage = () => {
-  const pathname = usePathname(); // Obtém o path atual
+  const pathname = usePathname();
 
   return (
-    <Link href={`${pathname.endsWith("/") ? pathname : pathname + "/"}vm-donalds`}>
-      Acesse VM Donalds clicando aqui!
-    </Link>
+    <div className="max-w-sm mx-auto mt-6 p-4 bg-gray-100 rounded-lg shadow-md">
+      <h1 className="text-xl font-bold text-gray-800 mb-3">Restaurantes Disponíveis</h1>
+      <p className="text-gray-600 mb-2">Confira abaixo as opções disponíveis:</p>
+
+      <ul className="space-y-2">
+        <li>
+          <Link
+            href={`${pathname.endsWith("/") ? pathname : pathname + "/"}vm-donalds`}
+            className="block bg-white p-3 rounded-md shadow-sm text-gray-800 font-medium transition hover:bg-gray-100"
+          >
+            🍔 VM Donalds
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
